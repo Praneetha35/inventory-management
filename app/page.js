71aaf3ebb8933911204c84112316b4bf0ebd45fe
Praneetha35 <praneethacp3535@gmail.com>
2024-08-05 14:32:24 -0400

    Added scan items and generate recipe feature using OpenAI

--- app/page.js
+++ app/page.js
@@ -2,9 +2,27 @@
 
 import { useState, useEffect } from "react";
 import { firestore } from "@/firebase";
-import { Box, Typography, Modal, Stack, TextField, Button, Card, CardContent, CircularProgress } from "@mui/material";
-import { collection, getDocs, query, setDoc, doc, getDoc, deleteDoc } from "firebase/firestore";
-import KitchenIcon from '@mui/icons-material/Kitchen'; // Pantry-related icon
+import {
+  Box,
+  Typography,
+  Modal,
+  Stack,
+  TextField,
+  Button,
+  Card,
+  CardContent,
+  CircularProgress,
+} from "@mui/material";
+import {
+  collection,
+  getDocs,
+  query,
+  setDoc,
+  doc,
+  getDoc,
+  deleteDoc,
+} from "firebase/firestore";
+import KitchenIcon from "@mui/icons-material/Kitchen"; // Pantry-related icon
 
 import ImageUploader from "./components/imageUploader";
 import { convertImageToBase64 } from "./utils/imageUtils";
@@ -22,10 +40,11 @@ export default function Home() {
   const [loadingImage, setLoadingImage] = useState(false);
   const [recipe, setRecipe] = useState("");
   const [loadingRecipe, setLoadingRecipe] = useState(false);
-
+  const [openRecipeModal, setOpenRecipeModal] = useState(false);
 
   const handleOpenImageModal = () => setOpenImageModal(true);
   const handleCloseImageModal = () => setOpenImageModal(false);
+  const handleCloseRecipeModal = () => setOpenRecipeModal(false);
 
   const handleImageUpload = (imageSrc) => {
     convertImageToBase64(imageSrc, async (imageBase64) => {
@@ -38,15 +57,15 @@ export default function Home() {
           },
           body: JSON.stringify({ imageBase64 }),
         });
-  
+
         if (!response.ok) {
           throw new Error("Failed to classify image");
         }
-  
+
         const data = await response.json();
         const itemName = data.label;
         console.log("itemName: ", itemName);
-        
+
         // setItemName(itemName)
         await addItem(itemName);
         setItemName("");
@@ -60,6 +79,7 @@ export default function Home() {
   };
 
   const handleGenerateRecipe = async () => {
+    setOpenRecipeModal(true);
     setLoadingRecipe(true);
     try {
       const response = await fetch("/api/generate-recipe", {
@@ -67,15 +87,16 @@ export default function Home() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ pantryItems: inventory.map((item) => item.name) }),
+        body: JSON.stringify({
+          pantryItems: inventory.map((item) => item.name),
+        }),
       });
-  
+
       if (!response.ok) {
         throw new Error("Failed to generate recipe");
       }
-  
+
       const data = await response.json();
-      console.log("recipe:",recipe);
       setRecipe(data.recipe);
     } catch (error) {
       console.error("Error generating recipe: ", error);
@@ -83,7 +104,6 @@ export default function Home() {
       setLoadingRecipe(false);
     }
   };
-  
 
   const updateInventory = async () => {
     setLoading(true);
@@ -108,26 +128,22 @@ export default function Home() {
 
   const removeItem = async (item) => {
     const docRef = doc(collection(firestore, "inventory"), item.toLowerCase());
-    const docSnap = await getDoc(docRef);
-
-    if (docSnap.exists()) {
-      const { quantity } = docSnap.data();
-      if (quantity === 1) {
-        await deleteDoc(docRef);
-      } else {
-        await setDoc(docRef, { quantity: quantity - 1 });
-      }
+    try {
+      // Delete the item from the collection
+      await deleteDoc(docRef);
+      await updateInventory();
+    } catch (error) {
+      console.error("Error removing item:", error);
     }
-    await updateInventory();
   };
 
   const addItem = async (productName) => {
     console.log("log inside: ", productName);
-    
+
     if (productName.trim() === "") return;
     const lowerCaseItemName = productName.trim().toLowerCase();
     console.log("lowerCaseItemName: ", lowerCaseItemName);
-    
+
     const docRef = doc(collection(firestore, "inventory"), lowerCaseItemName);
     const docSnap = await getDoc(docRef);
 
@@ -137,7 +153,7 @@ export default function Home() {
     } else {
       await setDoc(docRef, { quantity: newItemQuantity });
     }
-    setItemName('');
+    setItemName("");
     setNewItemQuantity(1);
     handleCloseAdd();
     await updateInventory();
@@ -146,11 +162,16 @@ export default function Home() {
   const updateItemQuantity = async () => {
     if (selectedItem === null || itemQuantity.trim() === "") return;
     const docRef = doc(collection(firestore, "inventory"), selectedItem.toLowerCase());
-    await setDoc(docRef, { quantity: parseInt(itemQuantity, 10) });
-    setItemQuantity('');
-    setSelectedItem(null);
-    handleCloseUpdate();
-    await updateInventory();
+    try {
+      // Update the quantity of the item
+      await setDoc(docRef, { quantity: parseInt(itemQuantity, 10) });
+      setItemQuantity("");
+      setSelectedItem(null);
+      handleCloseUpdate();
+      await updateInventory();
+    } catch (error) {
+      console.error("Error updating item quantity:", error);
+    }
   };
 
   const handleOpenAdd = () => setOpenAdd(true);
@@ -189,76 +210,68 @@ export default function Home() {
       >
         <KitchenIcon sx={{ fontSize: 40, color: "white" }} />
         <Typography variant="h4" fontWeight="bold" color="white">
-          Pantry Tracker
+          PantryPal
         </Typography>
         <Typography variant="subtitle1" color="white">
-          Your favorite food inventory management app
+          Simplify your kitchen, amplify your cooking
         </Typography>
       </Box>
-      <Box flex={1} padding={2} display="flex" flexDirection="column" alignItems="center">
-        <Stack direction="row" spacing={2} width="100%">
-      <Button
-        variant="contained"
-        sx={{ 
-          bgcolor: "#66bb6a", 
-          mt: 4, 
-          mb: 2, 
-          borderRadius: '20px', 
-          '&:hover': { bgcolor: "#4caf50" }, 
-          px: 4, 
-          py: 1 
-        }} // Subtle green color
-        onClick={handleOpenAdd}
+      <Box
+        flex={1}
+        padding={6}
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
       >
-        Add New Item
-      </Button>
+        <Stack direction="row" spacing={1} width="100%" mt = {2} mb = {2}>
+          <Button
+            variant="contained"
+            sx={{
+              bgcolor: "#66bb6a",
+              mt: 4,
+              mb: 1,
+              borderRadius: "20px",
+              "&:hover": { bgcolor: "#4caf50" },
+              px: 4,
+              py: 1,
+            }} // Subtle green color
+            onClick={handleOpenAdd}
+          >
+            Add New Item
+          </Button>
 
-      <Button
-        variant="contained"
-        sx={{ 
-          bgcolor: "#66bb6a", 
-          mt: 4, 
-          mb: 2, 
-          borderRadius: '20px', 
-          '&:hover': { bgcolor: "#4caf50" }, 
-          px: 4, 
-          py: 1 
-        }} // Subtle green color
-        onClick={handleOpenImageModal}
-      >
-        SCAN ITEM
-      </Button>
+          <Button
+            variant="contained"
+            sx={{
+              bgcolor: "#66bb6a",
+              mt: 4,
+              mb: 1,
+              borderRadius: "20px",
+              "&:hover": { bgcolor: "#4caf50" },
+              px: 4,
+              py: 1,
+            }} // Subtle green color
+            onClick={handleOpenImageModal}
+          >
+            SCAN ITEM
+          </Button>
 
-      <Button
-        variant="contained"
-        sx={{ 
-          bgcolor: "#66bb6a", 
-          mt: 4, 
-          mb: 2, 
-          borderRadius: '20px', 
-          '&:hover': { bgcolor: "#4caf50" }, 
-          px: 4, 
-          py: 1 
-        }} // Subtle green color
-        onClick={handleGenerateRecipe}
-      >
-        GENERATE RECIPE
-      </Button>
-      </Stack>
-      {loadingRecipe ? (
-          <CircularProgress sx={{ marginTop: "20px" }} />
-        ) : (
-          <Box border="1px solid #333" sx={{ width: "100%", padding: "20px", marginTop: "20px" }}>
-            <Typography variant="h4" color="#333" textAlign="center" marginBottom="20px">
-              Generated Recipe
-            </Typography>
-            <Typography variant="body1" color="#333" style={{ whiteSpace: "pre-wrap" }}>
-              {recipe
-                ? recipe
-                : "No recipe generated yet. Click the 'Generate Recipe' button to create a recipe."}
-            </Typography>
-          </Box>
-        )}
+          <Button
+            variant="contained"
+            sx={{
+              bgcolor: "#66bb6a",
+              mt: 4,
+              mb: 1,
+              borderRadius: "20px",
+              "&:hover": { bgcolor: "#4caf50" },
+              px: 4,
+              py: 1,
+            }} // Subtle green color
+            onClick={handleGenerateRecipe}
+          >
+            GENERATE RECIPE
+          </Button>
+        </Stack>
       </Box>
 
       <Modal open={openImageModal} onClose={handleCloseImageModal}>
@@ -279,31 +292,112 @@ export default function Home() {
           }}
         >
           {loadingImage ? (
-          <CircularProgress />
-        ) : (
-          <ImageUploader onImageUpload={handleImageUpload} />
-        )}
+            <CircularProgress />
+          ) : (
+            <ImageUploader onImageUpload={handleImageUpload} />
+          )}
+        </Box>
+      </Modal>
+      <Modal open={openRecipeModal} onClose={handleCloseRecipeModal}>
+        <Box
+          position="absolute"
+          top="50%"
+          left="50%"
+          width={600}
+          bgcolor="white"
+          border="2px solid #000"
+          borderRadius={2}
+          boxShadow={24}
+          p={4}
+          display="flex"
+          flexDirection="column"
+          gap={2}
+          sx={{
+            transform: "translate(-50%, -50%)",
+            maxHeight: "80vh",
+            overflowY: "auto",
+          }}
+        >
+          {loadingRecipe ? (
+            // Added CircularProgress inside the modal
+            <Box
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              flexDirection="column"
+              sx={{ flexGrow: 1 }}
+            >
+              <CircularProgress />
+              <Typography
+                variant="body2"
+                color="textSecondary"
+                sx={{ marginTop: 2 }}
+              >
+                Generating recipe, please wait...
+              </Typography>
+            </Box>
+          ) : (
+            <>
+              <Typography variant="h6" component="h2" gutterBottom>
+                Recipe Details
+              </Typography>
+              <Typography
+                variant="body1"
+                color="#333"
+                style={{ whiteSpace: "pre-wrap" }}
+              >
+                {recipe
+                  ? recipe
+                  : "No recipe generated yet. Click the 'Generate Recipe' button to create a recipe."}
+              </Typography>
+            </>
+          )}
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleCloseRecipeModal}
+          >
+            Close
+          </Button>
         </Box>
       </Modal>
 
-      <Box width="80%" p={4} bgcolor="white" borderRadius="8px" boxShadow={3} mt={2}>
+      <Box
+        width="80%"
+        p={2}
+        bgcolor="white"
+        borderRadius="8px"
+        boxShadow={3}
+        mt={1}
+        mb={9}
+      >
         <Typography variant="h5" fontWeight="bold" textAlign="center" mb={2}>
           Inventory Items
         </Typography>
         {loading ? (
-          <Box display="flex" justifyContent="center" alignItems="center" minHeight="200px">
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            minHeight="200px"
+          >
             <CircularProgress />
           </Box>
         ) : (
           <Stack spacing={2} overflow="auto">
             {inventory.map(({ name, quantity }) => (
-              <Card key={name} sx={{ backgroundColor: '#f1f8e9', boxShadow: 3 }}> {/* Very light green for cards */}
+              <Card
+                key={name}
+                sx={{ backgroundColor: "#f1f8e9", boxShadow: 3 }}
+              >
+                {" "}
+                {/* Very light green for cards */}
                 <CardContent
                   sx={{
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    alignItems: 'center',
-                    padding: '16px',
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    padding: "16px",
                   }}
                 >
                   <Typography variant="h6" fontWeight="medium">
@@ -316,12 +410,12 @@ export default function Home() {
                     <Button
                       variant="contained"
                       color="error"
-                      sx={{ 
-                        bgcolor: "#e57373", 
-                        borderRadius: '20px', 
-                        '&:hover': { bgcolor: "#ef5350" }, 
-                        px: 2, 
-                        py: 1 
+                      sx={{
+                        bgcolor: "#e57373",
+                        borderRadius: "20px",
+                        "&:hover": { bgcolor: "#ef5350" },
+                        px: 2,
+                        py: 1,
                       }} // Subtle red color for removal
                       onClick={() => removeItem(name)}
                     >
@@ -330,13 +424,13 @@ export default function Home() {
                     <Button
                       variant="outlined"
                       color="success"
-                      sx={{ 
-                        borderColor: "#66bb6a", 
-                        color: "#66bb6a", 
-                        borderRadius: '20px', 
-                        '&:hover': { borderColor: "#4caf50", color: "#4caf50" }, 
-                        px: 2, 
-                        py: 1 
+                      sx={{
+                        borderColor: "#66bb6a",
+                        color: "#66bb6a",
+                        borderRadius: "20px",
+                        "&:hover": { borderColor: "#4caf50", color: "#4caf50" },
+                        px: 2,
+                        py: 1,
                       }} // Subtle green for update
                       onClick={() => handleOpenUpdate(name)}
                     >
@@ -364,11 +458,13 @@ export default function Home() {
           flexDirection="column"
           gap={3}
           sx={{
-            transform: 'translate(-50%, -50%)',
-            borderRadius: '8px'
+            transform: "translate(-50%, -50%)",
+            borderRadius: "8px",
           }}
         >
-          <Typography variant="h6" fontWeight="medium">Add Item</Typography>
+          <Typography variant="h6" fontWeight="medium">
+            Add Item
+          </Typography>
           <Stack width="100%" direction="row" spacing={2}>
             <TextField
               variant="outlined"
@@ -389,12 +485,12 @@ export default function Home() {
               variant="contained"
               color="success"
               onClick={(e) => addItem(itemName)}
-              sx={{ 
-                bgcolor: "#66bb6a", 
-                borderRadius: '20px', 
-                '&:hover': { bgcolor: "#4caf50" }, 
-                px: 4, 
-                py: 1 
+              sx={{
+                bgcolor: "#66bb6a",
+                borderRadius: "20px",
+                "&:hover": { bgcolor: "#4caf50" },
+                px: 4,
+                py: 1,
               }} // Matching subtle green color
             >
               Add
@@ -417,11 +513,13 @@ export default function Home() {
           flexDirection="column"
           gap={3}
           sx={{
-            transform: 'translate(-50%, -50%)',
-            borderRadius: '8px'
+            transform: "translate(-50%, -50%)",
+            borderRadius: "8px",
           }}
         >
-          <Typography variant="h6" fontWeight="medium">Update Quantity</Typography>
+          <Typography variant="h6" fontWeight="medium">
+            Update Quantity
+          </Typography>
           <Stack width="100%" direction="row" spacing={2}>
             <TextField
               variant="outlined"
@@ -436,12 +534,12 @@ export default function Home() {
               variant="contained"
               color="success"
               onClick={updateItemQuantity}
-              sx={{ 
-                bgcolor: "#66bb6a", 
-                borderRadius: '20px', 
-                '&:hover': { bgcolor: "#4caf50" }, 
-                px: 4, 
-                py: 1 
+              sx={{
+                bgcolor: "#66bb6a",
+                borderRadius: "20px",
+                "&:hover": { bgcolor: "#4caf50" },
+                px: 4,
+                py: 1,
               }} // Matching subtle green color
             >
               Update
@@ -452,4 +550,3 @@ export default function Home() {
     </Box>
   );
 }
-
